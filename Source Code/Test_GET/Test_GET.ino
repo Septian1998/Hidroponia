fdbfc998ffb69b02bd3dd251687f028092cf1ad0
S. B. Triantino <septiantriantino@gmail.com>
2020-10-28 22:41:34 +0700

    Add Test GET

---
+++ Source Code/Test_GET/Test_GET.ino
@@ -0,0 +1,62 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "Bang Bud";
+const char* pass = "septian19";
+
+String serverName = "hidroponia-app.herokuapp.com/datafrontend";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, pass);
+    Serial.println("Connecting");
+    while(WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.println(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+    if((millis() - lastTime) > timerDelay)
+    {
+        if(WiFi.status() == WL_CONNECTED)
+        {
+            HTTPClient http;
+
+            String serverPath = serverName + "?field1=27";
+
+            http.begin(serverPath.c_str());
+
+            int httpResponseCode = http.GET();
+
+            if (httpResponseCode>0)
+            {
+                Serial.print("HTTP Response code: ");
+                Serial.println(httpResponseCode);
+                String payload = http.getString();
+                Serial.println(payload);
+            }
+            else
+            {
+                Serial.print("Error code: ");
+                Serial.println(httpResponseCode);
+            }
+            http.end();
+        }
+        else
+        {
+            Serial.println("WiFi Disconnected");
+        }
+        lastTime = millis();
+    }
+}
